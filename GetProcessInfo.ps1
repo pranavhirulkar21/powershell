b5143e5d1639f7dd86c00196f99d452971d30af0
pranavhirulkar21 <127311810+pranavhirulkar21@users.noreply.github.com>
2024-05-12 16:35:46 +0530

    Get process info part added along with renaming some file names

---
+++ GetProcessInfo.ps1
@@ -0,0 +1,45 @@
+﻿Write-Output "Process Management"
+
+$num = Read-Host "Select the option.`n
+1. Show all the processes. `n
+2. Search process by it's name. `n
+3. Search process by it's ID. `n
+4. Show 10 processes consuming amount most of CPU. `n
+5. Show 10 processes having most no. of handles.`n
+6. Show 10 processes consuming least amount of CPU. `n
+7. Show 10 processes having least no. of handles.`n `n "
+
+
+switch($num){
+1 {
+Get-Process
+}
+
+2 {
+$name = Read-Host "Enter the name of the process "
+Get-Process -Name $name
+}
+
+3 {
+$id = Read-Host "Enter the ID of the process "
+Get-Process -Id $id
+}
+
+4 {
+Get-Process | Sort-Object CPU -Descending | Select-Object -First 10
+}
+
+5 {
+Get-Process | Sort-Object Handles -Descending | Select-Object -First 10
+}
+
+6 {
+Get-Process | Sort-Object CPU -Descending | Select-Object -Last 10
+}
+
+7 {
+Get-Process | Sort-Object Handles -Descending | Select-Object -Last 10
+}
+
+default { Write-Output "Invalid Option !"}
+}
