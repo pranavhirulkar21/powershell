758a082da92a1d46b67ddc4b025213e42d040a5d
pranavhirulkar21 <127311810+pranavhirulkar21@users.noreply.github.com>
2024-05-12 19:25:34 +0530

    EventLog part added

---
+++ GetEventLogInfo.ps1
@@ -0,0 +1,42 @@
+﻿$num = Read-Host "
+1. View all available event logs on the system.
+2. View events from a specific event log.
+3. View events from a specific source.
+4. View events within a specific time range.
+5. View events with a specific event ID.
+6. View events based on a specific entry type.
+"
+
+switch ($num) {
+1 {
+Get-EventLog -List
+}
+
+2 {
+$logName = Read-Host "Enter the name of the event log (e.g., Application, System)"
+Get-EventLog -LogName $logName
+}
+
+3 {
+$source = Read-Host "Enter the name of the event source"
+Get-EventLog | Where-Object {$_.Source -eq $source}
+}
+
+4 {
+$startTime = Read-Host "Enter the start time (e.g., 'MM/dd/yyyy hh:mm:ss')"
+$endTime = Read-Host "Enter the end time (e.g., 'MM/dd/yyyy hh:mm:ss')"
+Get-EventLog -After $startTime -Before $endTime
+}
+
+5 {
+$eventID = Read-Host "Enter the event ID"
+Get-EventLog -InstanceId $eventID
+}
+
+6 {
+$entryType = Read-Host "Enter the entry type (e.g., Error, Warning, Information)"
+Get-EventLog -EntryType $entryType
+}
+
+default { Write-Output "Invalid Input!" }
+}
